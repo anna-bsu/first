a48de4e30ac032fe3b90ef4e765d7b0720277d6d
annabsu <dorohovich@bsu.by>
2019-08-08 00:26:20 +0300

    the initial state

--- task 4,5/index.js.js
+++
@@ -1,7 +0,0 @@
-module.exports = createAutocomplete;
-function createAutocomplete(words){
-	return function (word){
-		      if( word === '' || null || undefined) return [];
-              return words.filter(item => item.toLowerCase().indexOf(word.toLowerCase(word)) === 0);
-	        }
-}
