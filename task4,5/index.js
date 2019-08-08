94d2783191e7f28a391d71d34219fd5535c6c543
anna-bsu <52789649+anna-bsu@users.noreply.github.com>
2019-08-08 15:15:42 +0300

    final version autocomplete

--- task4,5/index.js
+++ task4,5/index.js
@@ -1,6 +1,6 @@
 function createAutoComplete(words){
 	return function (word){
-		      if( word === '' || null || undefined) {
+		      if( word === '' || word === undefined) {
 				  return [];
 			  }
               return words.filter(item => item.toLowerCase().indexOf(word.toLowerCase(word)) === 0);
